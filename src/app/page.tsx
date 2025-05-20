28c4fbd9d3cd166da02f8f63cc85745af3962a9f
Elliot Vaucher <67040832+elliotvaucher@users.noreply.github.com>
2025-05-20 09:23:21 +0200

    change branding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,7 @@ function Home() {
       <footer className="my-4 text-center text-sm text-gray-600 print:hidden">
         <a href="https://github.com/u14app/" target="_blank">
           {t("copyright", {
-            name: "U14App",
+            name: "RITSL",
           })}
         </a>
       </footer>
